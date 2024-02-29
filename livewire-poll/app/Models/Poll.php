5883b08018859df447e63f1b5d78d5fa9a41f120
Zest00 <alidawood098@gmail.com>
2024-02-29 09:47:42 +0200

    Create livewire poll

---
+++ livewire-poll/app/Models/Poll.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Poll extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['title'];
+
+    protected function poll():BelongsTo
+    {
+        return $this->belongsTo(Poll::class);
+    }
+
+    public function options(): HasMany
+    {
+        return $this->hasMany(Option::class);
+    }
+}
